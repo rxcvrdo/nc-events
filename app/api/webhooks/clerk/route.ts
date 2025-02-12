d85386d995be33f7321378cab28a91d0cf7438a4
rxcvrdo <rmurdock177@hotmail.co.uk>
2025-02-12 14:42:15 +0000

    updated route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs/server'
+import { clerkClient } from '@clerk/clerk-sdk-node'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
